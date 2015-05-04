8fe707a40e52375e93ae2df06128459979d30dc4
Lígia Araújo <maria_ligia16@hotmail.com>
2015-05-04 15:51:03 -0300

    Atualização
    Atualização da Classe Banco

--- src/java/banco/Banco.java
+++ src/java/banco/Banco.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -21,6 +22,7 @@ public class Banco {
     private Statement stmt;
     private ResultSet rs;
     public Connection conn;
+    private static banco.Banco b = new banco.Banco();
 
     public Banco() {
         String driver = "com.mysql.jdbc.Driver";
@@ -34,37 +36,10 @@ public class Banco {
             stmt = (Statement) conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
-}
-}
-         public Connection getConn() {
-        return conn;
-    
-
         }
+    }
 
-   public void cadastroAluno(String nome, String matricula, String dataNasc, String email, String telefone, String cpf, String sexo) throws SQLException {
-        PreparedStatement ps = conn.prepareStatement("insert into aluno (nome, matricula, dataNasc, email, telefone, cpf, sexo) values ('" + nome + "', '" + matricula + "', '" + dataNasc + "', '" + email + "', '" + telefone  + "',  '" + cpf  + "',  '" + sexo +"');");
-        ps.execute();
-        ps.close();
-    
-   }
-    public void cadastrarRefeicao (String nome, String matricula, String tipoRefeicao, String justificativa) throws SQLException {
-        PreparedStatement ps = conn.prepareStatement("insert into refeicao (nome, matricula, tipoRefeicao, justificativa) values ('" + nome + "', '" + matricula + "', '" + tipoRefeicao + "',  '" +justificativa +"');");
-        ps.execute();
-        ps.close();
-  
-   }
-       public void ReservaEsporte (String nome, String matricula, String horario, String tipoEsporte, String qtdBolas, String turno) throws SQLException {
-        PreparedStatement ps = conn.prepareStatement("insert into esporte (nome, matricula, horario, tipoEsporte, qtdBolas, turno) values ('" + nome + "', '" + matricula + "', '" + horario + "', '" + tipoEsporte + "', '" + qtdBolas  + "',  '" + turno +"');");
-        ps.execute();
-        ps.close();
-  
+    public Connection getConn() {
+        return conn;
+    }
 }
-         public void ReservarConsulta (String nome, String matricula, String curso, String espMed, String horario) throws SQLException {
-        PreparedStatement ps = conn.prepareStatement("insert into constmed (nome, matricula, curso, espMed, horario) values ('" + nome + "', '" + matricula + "', '" + curso + "', '" + espMed + "', '" + horario + "');");
-        ps.execute();
-        ps.close();
-         }
-
-}
-  
